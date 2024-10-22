f0b3579a1571f6a7b5fcc9be5d892467054635ec
Teh SoTo <sansoto5000@gmail.com>
2024-10-22 18:32:12 -0300

    Initial commit

---
+++ app/stocks/page.tsx
@@ -0,0 +1,23 @@
+// app/stocks/page.js
+import fs from "fs";
+import path from "path";
+import StocksPageContent from "./components/stocks-page.content"; // Adjust the import path as needed
+
+// Helper to read all stock JSON files from the content folder
+const getAllStocks = () => {
+  const contentDir = path.join(process.cwd(), "app", "stocks", "content");
+  const files = fs.readdirSync(contentDir);
+
+  const stocks = files.map((file) => {
+    const filePath = path.join(contentDir, file);
+    const fileContent = fs.readFileSync(filePath, "utf-8");
+    return JSON.parse(fileContent);
+  });
+
+  return stocks;
+};
+
+export default function Page() {
+  const stocks = getAllStocks();
+  return <StocksPageContent stocks={stocks} />;
+}
---
+++ next.config.js
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+
+
+/** @type {import("next").NextConfig} */
+const config = {};
+
+
+export default config;
